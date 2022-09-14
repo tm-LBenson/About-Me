2bb549b95e645495aa05487ef16f97ffab531f0f
Lewis Benson <tm.lewis.benson@gmail.com>
2022-09-14 16:24:51 -0400

    add question 6; the guessing game

--- js/app.js
+++ js/app.js
@@ -5,7 +5,7 @@ let truckQuestion = '';
 let sportsQuestion = '';
 let presQuestion = '';
 let userScore = 0;
-
+let numGameGuesses = 4;
 
 let usersName = prompt('What is your name?').toLowerCase().split('');
 usersName[0] = usersName[0].toUpperCase();
@@ -16,6 +16,7 @@ alert(`Welcome to my site ${usersName}`);
 while(!petsQuestion){
   petsQuestion = prompt('Do I have any pets? yes or no?').toLowerCase();
   if (petsQuestion[0] === 'y'){
+    userScore++;
     alert('Correct! I have a cat!');
   }else if(petsQuestion[0] === 'n'){
   // console.log('Incorrect! I have a cat!');
@@ -30,6 +31,7 @@ while(!petsQuestion){
 while(!childQuestion){
   childQuestion = prompt('Do I a daughter? yes or no?').toLowerCase();
   if (childQuestion[0] === 'y'){
+    userScore++;
     // console.log('Correct! I have a daughter!');
     alert('Correct! I have a daughter!');
   }else if(childQuestion[0] === 'n'){
@@ -48,6 +50,7 @@ while(!truckQuestion){
     // console.log('Incorrect! I have a car!');
     alert('Incorrect! I have a car!');
   }else if(truckQuestion[0] === 'n'){
+    userScore++;
     // console.log('Correct! I have a car!');
     alert('Correct! I have a car!');
   }else{
@@ -63,6 +66,7 @@ while(!sportsQuestion){
     // console.log('Incorrect! I have little interest with sports!');
     alert('Incorrect! I have little interest with sports!');
   }else if(sportsQuestion[0] === 'n'){
+    userScore++;
     // console.log('Correct! I have little interest with sports!');
     alert('Correct! I have little interest with sports!');
   }else{
@@ -78,6 +82,7 @@ while(!presQuestion){
     // console.log('Incorrect! I have never met the President!');
     alert('Incorrect! I have never met the President!');
   }else if(presQuestion[0] === 'n'){
+    userScore++;
     // console.log('Correct! I have never met the President!');
     alert('Correct! I have never met the President!');
   }else{
@@ -86,4 +91,29 @@ while(!presQuestion){
     alert('Invalid response');
   }
 }
-alert(`Thanks for visiting my site ${usersName}`);
+
+
+let gameOn = true;
+while(gameOn === true && numGameGuesses >= 0){
+
+  let guessingGame = Number(prompt(`You have ${numGameGuesses} guesses to answer how many musical instruments I own.`));
+  if (isNaN(guessingGame)){
+    alert('Not a valid number!');
+  }else if(guessingGame === 12){
+    userScore++;
+    alert('12 is correct!');
+    gameOn = false;
+  }else if(guessingGame < 12){
+    alert('Higher!');
+  }else if(guessingGame > 12){
+    alert('Lower!');
+  }
+  numGameGuesses--;
+  if (numGameGuesses === 0){
+    alert('I own 12 musical instruments!');
+    break;
+  }
+}
+
+
+alert(`Thanks for visiting my site ${usersName} your score is ${userScore}!`);
